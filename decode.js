da94de114359a071bdd0091ba436479a9a46e37c
Chandler Prince <david.prince@wust.edu>
2020-03-11 13:15:35 -0500

    renamed Functions to be more selfDocumenting

--- decode.js
+++ decode.js
@@ -3,28 +3,28 @@ let inlayCode, bridgeCode, colorCode, hardwareCode, treblepuCode, middlepuCode, 
 let model, topWood, frets, topSpec, topGrade, neckWood, neckCarve, fingerboard;
 let inlay, bridge, color, hardware, treblepu, middlepu, basspu, elec;
 
-const setValues = (field, value) => document.getElementById(field).innerHTML = value;
+const addValueToDOM = (field, value) => document.getElementById(field).innerHTML = value;
 
-const setFieldValues = () => {
-    setValues("modelField", model);
-    setValues("topWoodField", topWood);
-    setValues("fretsField", frets);
-    setValues("topSpecField", topSpec);
-    setValues("topGradeField", topGrade);
-    setValues("neckWoodField", neckWood);
-    setValues("neckCarveField", neckCarve);
-    setValues("fingerboardField", fingerboard);
-    setValues("inlayField", inlay);
-    setValues("bridgeField", bridge);
-    setValues("colorField", color);
-    setValues("hardwareField", hardware);
-    setValues("treblepuField", treblepu);
-    setValues("middlepuField", middlepu);
-    setValues("basspuField", basspu);
-    setValues("elecField", elec)
+const addFieldValuesToDOM = () => {
+    addValueToDOM("modelField", model);
+    addValueToDOM("topWoodField", topWood);
+    addValueToDOM("fretsField", frets);
+    addValueToDOM("topSpecField", topSpec);
+    addValueToDOM("topGradeField", topGrade);
+    addValueToDOM("neckWoodField", neckWood);
+    addValueToDOM("neckCarveField", neckCarve);
+    addValueToDOM("fingerboardField", fingerboard);
+    addValueToDOM("inlayField", inlay);
+    addValueToDOM("bridgeField", bridge);
+    addValueToDOM("colorField", color);
+    addValueToDOM("hardwareField", hardware);
+    addValueToDOM("treblepuField", treblepu);
+    addValueToDOM("middlepuField", middlepu);
+    addValueToDOM("basspuField", basspu);
+    addValueToDOM("elecField", elec)
 };
 
-const setCodeValues = str => {
+const getCodesFromString = str => {
     str = format(str);
     modelCode = str.substring(0, 2);
     topWoodCode = str.substring(2, 3);
@@ -44,38 +44,38 @@ const setCodeValues = str => {
     elecCode = str.substring(19, 20);
 };
 
-const getMODCATstring = (string, array, specName) => {
-    const modcatKey = string + array;
+const getObjValue = (string, code, specName) => {
+    const modcatKey = string + code;
     if(prs[modcatKey]){
         return prs[modcatKey];
     } else {
-        return "Could not find " + specName + " '" + array + "'"
+        return "Could not find " + specName + " '" + code + "'"
     }
 };
 
-const setMODCATValues = () => {
-    model = getMODCATstring("model", modelCode, "Model");
-    topWood = getMODCATstring("topWood", topWoodCode, "Top Wood");
-    frets = getMODCATstring("frets", fretsCode, "Frets");
-    topSpec = getMODCATstring("topSpec", topSpecCode, "Top Spec");
-    topGrade = getMODCATstring("topGrade", topGradeCode, "Top Grade");
-    neckWood = getMODCATstring("neckWood", neckWoodCode, "Neck Wood");
-    neckCarve = getMODCATstring("neckCarve", neckCarveCode, "Neck Carve");
-    fingerboard = getMODCATstring("fingerboard", fingerboardCode, "Fingerboard Wood");
-    inlay = getMODCATstring("inlay", inlayCode, "Inlay");
-    bridge = getMODCATstring("bridge", bridgeCode, "Bridge");
-    color = getMODCATstring("color", colorCode, "Color");
-    hardware = getMODCATstring("hardware", hardwareCode, "Hardware");
-    treblepu = getMODCATstring("treblepu",treblepuCode, "Treble Pickup");
-    middlepu = getMODCATstring("middlepu",middlepuCode, "Middle Pickup");
-    basspu = getMODCATstring("basspu", basspuCode, "Bass Pickup");
-    elec = getMODCATstring("elec", elecCode, "Electronics");
+const getObjValues = () => {
+    model = getObjValue("model", modelCode, "Model");
+    topWood = getObjValue("topWood", topWoodCode, "Top Wood");
+    frets = getObjValue("frets", fretsCode, "Frets");
+    topSpec = getObjValue("topSpec", topSpecCode, "Top Spec");
+    topGrade = getObjValue("topGrade", topGradeCode, "Top Grade");
+    neckWood = getObjValue("neckWood", neckWoodCode, "Neck Wood");
+    neckCarve = getObjValue("neckCarve", neckCarveCode, "Neck Carve");
+    fingerboard = getObjValue("fingerboard", fingerboardCode, "Fingerboard Wood");
+    inlay = getObjValue("inlay", inlayCode, "Inlay");
+    bridge = getObjValue("bridge", bridgeCode, "Bridge");
+    color = getObjValue("color", colorCode, "Color");
+    hardware = getObjValue("hardware", hardwareCode, "Hardware");
+    treblepu = getObjValue("treblepu",treblepuCode, "Treble Pickup");
+    middlepu = getObjValue("middlepu",middlepuCode, "Middle Pickup");
+    basspu = getObjValue("basspu", basspuCode, "Bass Pickup");
+    elec = getObjValue("elec", elecCode, "Electronics");
 };
 // removes whitespace replaces - with _ and everything is made uppercase
 const format = text => text.replace(/\s+/g,"").replace(/-/g, "_").toUpperCase();
 
-const decodeMODCAT = code => {
-    setCodeValues(code);
-    setMODCATValues();
-    setFieldValues()
+const decodeMODCAT = input => {
+    getCodesFromString(input);
+    getObjValues();
+    addFieldValuesToDOM();
 };
